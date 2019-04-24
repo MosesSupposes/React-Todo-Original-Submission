3f536c5cea466195602377e41274d9f5ff0f48e2
Moses Samuel <mosamuel98@gmail.com>
2019-04-24 15:20:59 -0700

    refactor: lift applyClasses invocation to TodoList

--- src/components/TodoComponents/Todo.jsx
+++ src/components/TodoComponents/Todo.jsx
@@ -3,16 +3,8 @@ import React from 'react'
 
 export default function Todo(props) {
     return (
-        <div className={applyClasses(props.classList)}> 
+        <div className={props.classList}> 
             <p>{props.content}</p>
         </div>
     )
 }
-
-
-// ---- Helpers ----
-function applyClasses(classList) {
-    return classList.join(' ')
-}
-
-applyClasses(['green', 'large'])
